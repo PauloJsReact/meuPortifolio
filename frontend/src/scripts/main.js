c847ba0c135646b6b38f0f1f139563ebdeeb818c
PauloJsReact <paulo_henrique_araujo@outlook.com>
2022-01-15 20:02:50 -0300

    The page navigation and as animations created

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,11 +1,28 @@
-var dropTitle = document.querySelectorAll(".ph-text-title h1");
 
-window.addEventListener("load", function () {
+AOS.init();
 
 
+function removeAnimHome() {
     for (let i = 0; i < dropTitle.length; i++) {
-        dropTitle[i].classList.add("animate__animated");
-        dropTitle[i].classList.add("animate__bounceInLeft");
+        dropTitle[i].classList.remove("animate__animated");
+        dropTitle[i].classList.remove("animate__bounceInLeft");
     }
+}
 
-})
+/* Functions to nav menu */
+
+function navHome() {
+    window.location.href = "#home";
+
+}
+
+function navSobre() {
+    window.location.href = "#sobre";
+
+}
+
+function navProjects() {
+    window.location.href = "#projetos";
+
+}
+
