810cd427cc552c89e9b34d2e0f85f2ae26a0edfc
PauloJsReact <paulo_henrique_araujo@outlook.com>
2022-01-15 21:05:26 -0300

    New opdate

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,41 +1,19 @@
-var dropTitle = document.querySelectorAll(".ph-text-title h1");
-
 AOS.init();
-
-window.addEventListener("load", function () {
-    addAnimHome()
-});
-
-function addAnimHome() {
-    for (let i = 0; i < dropTitle.length; i++) {
-        dropTitle[i].classList.add("animate__animated");
-        dropTitle[i].classList.add("animate__bounceInLeft");
-    }
-}
-function removeAnimHome() {
-    for (let i = 0; i < dropTitle.length; i++) {
-        dropTitle[i].classList.remove("animate__animated");
-        dropTitle[i].classList.remove("animate__bounceInLeft");
-    }
-}
 
 /* Functions to nav menu */
 
 function navHome() {
-    addAnimHome()
-    window.location.href = "#home";
+    window.location.href = "#";
 
 }
 
 function navSobre() {
     window.location.href = "#sobre";
-    removeAnimHome()
 
 }
 
 function navProjects() {
     window.location.href = "#projetos";
-    removeAnimHome()
 
 }
 
