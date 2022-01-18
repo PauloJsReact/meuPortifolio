1d7d5cb760087bad2a624b305b6ec9dbb2f89872
PauloJsReact <paulo_henrique_araujo@outlook.com>
2022-01-17 22:35:50 -0300

    Slaide created

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,5 +1,6 @@
+var dataBase = "https://testjsoncostrut.firebaseio.com/projects.json"
+var projBar = document.querySelector(".ph-proj-bar");
 AOS.init();
-
 /* Functions to nav menu */
 
 function navHome() {
@@ -14,6 +15,51 @@ function navSobre() {
 
 function navProjects() {
     window.location.href = "#projetos";
-
 }
 
+
+/**  Functions for slaider  **/
+var table;
+
+async function chargeProjects() {
+    const res = await fetch(dataBase);
+    table = await res.json();
+
+    if (table != "") {
+
+        slaiders()
+
+    } else {
+
+    }
+}
+
+var index = 0
+function slaiders() {
+    projBar.innerHTML = `<img src='${table[index].img}'  class='animate__animated animate__flipInY'/>`
+}
+
+
+function nextSlaide() {
+    if (index < table.length) {
+        index = index + 1
+        projBar.innerHTML = `<img src='${table[index].img}'  class='animate__animated animate__flipInY'/>`
+    } else {
+        index = 0
+        projBar.innerHTML = `<img src='${table[index].img}'  class='animate__animated animate__flipInY'/>`
+    }
+    console.log(index)
+}
+
+function firstSlaide() {
+    if (index < table.length) {
+        index = index - 1
+        projBar.innerHTML = `<img src='${table[index].img}'  class='animate__animated animate__zoomInRight'/>`
+        console.log(table[index].img)
+    } else {
+        projBar.innerHTML = `<img src='${table[index].img}'  class='animate__animated animate__zoomInRight'/>`
+    }
+    console.log(index)
+}
+
+chargeProjects();
